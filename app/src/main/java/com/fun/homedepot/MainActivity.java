09f5230f0ba6ec6e50454478a651296c6e663ddb
Rui <liurui320916@gmail.com>
2018-07-30 13:10:25 -0500

    Initial commit

---
+++ app/src/main/java/com/fun/homedepot/MainActivity.java
@@ -0,0 +1,79 @@
+package com.fun.homedepot;
+
+import android.content.Context;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.mindorks.placeholderview.SwipeDecor;
+import com.mindorks.placeholderview.SwipeDirectionalView;
+import com.mindorks.placeholderview.listeners.ItemRemovedListener;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, TinderCard.Callback{
+    private GoogleMap mGoogleMap;
+    private SwipeDirectionalView mSwipeView;
+    private Context mContext;
+    private int mAnimationDuration = 300;
+    private boolean isToUndo = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSwipeView = (SwipeDirectionalView) findViewById(R.id.swipeView);
+        mContext = getApplicationContext();
+
+        int bottomMargin = Utils.dpToPx(160);
+        Point windowSize = Utils.getDisplaySize(getWindowManager());
+        mSwipeView.getBuilder()
+                .setDisplayViewCount(3)
+                .setIsUndoEnabled(true)
+                .setSwipeVerticalThreshold(Utils.dpToPx(50))
+                .setSwipeHorizontalThreshold(Utils.dpToPx(50))
+                .setHeightSwipeDistFactor(10)
+                .setWidthSwipeDistFactor(5)
+                .setSwipeDecor(new SwipeDecor()
+                        .setViewWidth(windowSize.x)
+                        .setViewHeight(windowSize.y - bottomMargin)
+                        .setViewGravity(Gravity.TOP)
+                        .setPaddingTop(20)
+                        .setSwipeAnimTime(mAnimationDuration)
+                        .setRelativeScale(0.01f)
+                        .setSwipeInMsgLayoutId(R.layout.tinder_swipe_in_msg_view)
+                        .setSwipeOutMsgLayoutId(R.layout.tinder_swipe_out_msg_view));
+
+
+        Point cardViewHolderSize = new Point(windowSize.x, windowSize.y - bottomMargin);
+
+        for (Profile profile : Utils.loadProfiles(this.getApplicationContext())) {
+            mSwipeView.addView(new TinderCard(mContext, profile, cardViewHolderSize, this));
+        }
+
+        mSwipeView.addItemRemoveListener(new ItemRemovedListener() {
+            @Override
+            public void onItemRemoved(int count) {
+                if (isToUndo) {
+                    isToUndo = false;
+                    mSwipeView.undoLastSwipe();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onSwipeUp() {
+        Toast.makeText(this, "SUPER LIKE! Show any dialog here.", Toast.LENGTH_SHORT).show();
+        isToUndo = true;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+
+    }
+}
